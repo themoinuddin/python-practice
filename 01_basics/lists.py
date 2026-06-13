e3334562d0c789bc615304002a657270e553c1de
themoinuddin <moinuddin.digital@gmail.com>
2026-06-13 23:19:49 +0500

    Add lists.py - list operations

---
+++ 01_basics/lists.py
@@ -0,0 +1,15 @@
+# lists.py
+# Author: Moin Uddin
+
+fruits = ["apple", "banana", "mango"]
+
+print(fruits[0])
+print(fruits[-1])
+
+fruits.append("orange")
+print(fruits)
+
+fruits.remove("banana")
+print(fruits)
+
+print(len(fruits))
