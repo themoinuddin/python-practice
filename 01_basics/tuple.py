602144bcc5028082a5cdfbf4e031c27e1761c708
themoinuddin <moinuddin.digital@gmail.com>
2026-06-18 15:49:32 +0500

    Add tuple.py - tuples in Python

---
+++ 01_basics/tuple.py
@@ -0,0 +1,17 @@
+# tuple.py
+# Author: Moin Uddin
+
+fruits = ("apple", "banana", "mango")
+
+print(fruits)
+print(fruits[0])
+print(fruits[-1])
+print(len(fruits))
+
+# loop
+for fruit in fruits:
+    print(fruit)
+
+# tuple unpacking
+a, b, c = fruits
+print(a, b, c)
