e4771da1eccc2e8e3473c88362b36d31566dcacd
themoinuddin <moinuddin.digital@gmail.com>
2026-06-19 00:19:50 +0500

    Add operator_overloading.py - overloading operators

---
+++ 02_oop/operator_overloading.py
@@ -0,0 +1,21 @@
+# operator_overloading.py
+# Author: Moin Uddin
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __str__(self):
+        return f"Point({self.x}, {self.y})"
+
+p1 = Point(1, 2)
+p2 = Point(3, 4)
+p3 = p1 + p2
+
+print(p1)
+print(p2)
+print(p3)
