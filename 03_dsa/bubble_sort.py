61c54d758fa41e4837c30323a867df9eb180997c
themoinuddin <moinuddin.digital@gmail.com>
2026-06-19 00:00:06 +0500

    Add bubble_sort.py - bubble sort algorithm

---
+++ 03_dsa/bubble_sort.py
@@ -0,0 +1,15 @@
+# bubble_sort.py
+# Author: Moin Uddin
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+arr = [5, 3, 1, 4, 2]
+print("Before:", arr)
+bubble_sort(arr)
+print("After:", arr)
