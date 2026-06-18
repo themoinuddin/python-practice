13e9e957f834c728a1f6bdcf335e77fbfd118a09
themoinuddin <moinuddin.digital@gmail.com>
2026-06-19 00:10:47 +0500

    Add type_casting.py - type conversion in Python

---
+++ 01_basics/type_casting.py
@@ -0,0 +1,22 @@
+# type_casting.py
+# Author: Moin Uddin
+
+# string to int
+age = int("19")
+print(age, type(age))
+
+# string to float
+gpa = float("3.8")
+print(gpa, type(gpa))
+
+# int to string
+name = str(100)
+print(name, type(name))
+
+# int to float
+x = float(5)
+print(x, type(x))
+
+# float to int
+y = int(3.9)
+print(y, type(y))
