0fc3f153693a502bf48dd465cba5a0f63aaadbd7
themoinuddin <moinuddin.digital@gmail.com>
2026-06-19 00:31:54 +0500

    Add selection_sort.py - selection sort algorithm

---
+++ 03_dsa/selection_sort.py
@@ -0,0 +1,16 @@
+# selection_sort.py
+# Author: Moin Uddin
+
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+arr = [5, 3, 1, 4, 2]
+print("Before:", arr)
+selection_sort(arr)
+print("After:", arr)
