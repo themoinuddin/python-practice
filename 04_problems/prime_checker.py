d91ba1981bd261739f75c92b1a25e88e999b0a9c
themoinuddin <moinuddin.digital@gmail.com>
2026-06-24 18:55:09 +0500

    Add prime number checker to 04_problems

---
+++ 04_problems/prime_checker.py
@@ -0,0 +1,16 @@
+# Author: Moin Uddin
+# Prime Number Checker
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+num = int(input("Enter a number: "))
+if is_prime(num):
+    print(f"{num} is a Prime Number")
+else:
+    print(f"{num} is not a Prime Number")
